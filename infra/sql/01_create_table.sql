7658d1c655a9c6ca009e027e6a96fa2a2630fe69
Gustavo Silva <psngustavo.santos@gmail.com>
2025-11-26 11:42:33 -0300

    Edit Redme

--- infra/sql/01_create_table.sql
+++ infra/sql/01_create_table.sql
@@ -11,16 +11,3 @@ CREATE TABLE IF NOT EXISTS public.tb_sensor_evento (
     ALERTA BOOLEAN,
     DT_CARGA TIMESTAMP DEFAULT NOW()
 );
-
---     ID SERIAL PRIMARY KEY,
---     device_id TEXT,
---     timestamp_ts TIMESTAMP,
---     temperature DOUBLE PRECISION,
---     humidity DOUBLE PRECISION,
---     latitude DOUBLE PRECISION,
---     longitude DOUBLE PRECISION,
---     status TEXT,
---     battery DOUBLE PRECISION,
---     alert BOOLEAN,
---     dt_carga TIMESTAMP DEFAULT NOW()
--- );
